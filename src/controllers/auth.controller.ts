14fad1d5e586341bb2deece541afed0c0bf37f75
ShayeGun <shayegan.7200@gmail.com>
2023-10-24 23:19:34 +0330

    feature/implement caching (redis) + secure compose

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,9 +1,17 @@
 import { catchAsync } from "../utils/catch-async";
 import { Request, Response, NextFunction } from "express";
 import { User } from "../models/user.model";
+import { redis } from "../utils/cache-redis";
 
 export const preRegisterUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const { phoneNumber } = req.body;
     const user = await User.create({ phoneNumber });
     res.json(user);
+});
+
+export const cacheOtp = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const { phoneNumber } = req.query;
+    await redis.set(phoneNumber as string, '123');
+    const v = await redis.get(phoneNumber as string);
+    res.json({ data: v });
 });
--- src/routes/index-router.ts
+++ src/routes/index-router.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { AuthController } from "../controllers";
-import { genericValidator } from "../middlewares/request-checker";
+import { genericValidator, FieldType } from "../middlewares/request-checker";
 import * as Dto from '../utils/validator-checker/index';
 const router = Router();
 
 router.route('/user/pre-register')
     .post(genericValidator(Dto.CreateUserDto), AuthController.preRegisterUser);
+
+router.route('/user/otp')
+    .get(genericValidator(Dto.CreateUserDto, FieldType.QUERY), AuthController.cacheOtp);
 
 export { router as mainRouter };
